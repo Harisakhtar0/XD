38e9c245746dd75584f2e13056cfe1c07fa644f1
Harisakhtar <109353887+Harisakhtar0@users.noreply.github.com>
2023-03-10 20:58:31 +0500

    Update Harry.py

--- Harry.py
+++ Harry.py
@@ -1,7 +1,25 @@
-import os,re,sys
+import os, platform
 
 os.system("xdg-open https://www.facebook.com/groups/893149411835087/?ref=share")
 
-import nonofix
+os.system('git pull')
 
-nonofix.menu()
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from nonofix import menu
+
+    menu()
+
+elif bit == '32bit':
+
+    print("NOT UPDATED YET BRO ")
+
+
+
+
+
+
